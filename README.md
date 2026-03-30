c5d7a35d6b330c2c274aaf7a6697c149de5fc7a5
Abel Almaraz <almarazabeljesus@gmail.com>
2026-03-30 16:56:51 -0300

    ejercicio 1

---
+++ README.md
@@ -0,0 +1,22 @@
+# Trabajo Prácticas Funciones y Array
+
+## Descripción 📝
+Este trabajo práctico contiene una serie de ejercicios básicos de **JavaScript** orientados a practicar el uso de **Programacion Orientada a Objetos**.
+
+Los ejercicios están organizados por niveles de dificultad y ayudan a comprender conceptos fundamentales de programación.
+
+---
+
+## Algunas Funciones vistas
+-.................
+
+- JavaScript
+- HTML (para ejecutar los scripts en el navegador)
+## Autor
+Trabajo práctico realizado por **Jesus Abel Almaraz**.
+
+## Pasos parar probar el proyecto📌
+Segui los siguientes pasos en una terminal para probar localmente el proyecto.
+`https://github.com/AlmarazAbel/Tp4.git`
+
+cd 13-TpProgramacionOrientadaObjetos
--- js/1.js
+++ js/1.js
@@ -1,1 +1,33 @@
-//
+/*1- Crea un objeto llamado auto que tenga algunas características como el 
+color, marca, modelo y si está encendido o apagado. Crea los métodos 
+ necesarios para permitir encender y apagar el auto.
+Output:
+
+objeto.encender();
+objeto.apadar()
+auto encendido
+El auto se apagó */
+
+// Crear el objeto auto
+const auto = {
+    //propiedades
+    color: "gris oscuro",
+    marca: "Ford",
+    modelo: "Focus",
+    encendido: false,
+//metodos
+    encender: function() {
+        this.encendido = true;
+        document.getElementById("pantalla").innerHTML += "auto encendido";
+    },
+
+    apagar: function() {
+        this.encendido = false;
+        document.getElementById("pantalla").innerHTML += "<br>el auto se apagò";
+    }
+};
+
+// Uso de los métodos
+auto.encender();
+auto.apagar();
+
